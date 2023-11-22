b1cd9e205ca31aaf0f7e76c15ce3908743c39cd3
YasserCristanchoDiaz <yasser.cristancho@uptc.edu.co>
2023-11-21 23:04:02 -0500

    registro y consultar

--- routes/index.js
+++ routes/index.js
@@ -3,4 +3,12 @@ router.get('/',(req,res)=>{
     res.render('index')
 });
 
+router.get('/register',(req,res)=>{
+    res.render('formRegister')
+});
+
+router.get('/consult',(req,res)=>{
+    res.render('consultTable')
+});
+
 module.exports=router;
