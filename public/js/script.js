a13201ee1bbf4753348f4bece5a2da2843864f74
YasserCristanchoDiaz <yasser.cristancho@uptc.edu.co>
2023-11-25 13:35:19 -0500

    front eliminar, se agrego sweetalert2.all.js para ms de confirmacion antes de eliminar

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,4 @@
-const addBtn = document.getElementById('add-btn')
+
 const nameP = document.getElementById('name')
 const description = document.getElementById('description')
 const email = document.getElementById('email')
@@ -9,6 +9,58 @@ const modality = document.getElementById('select')
 const nameError = document.getElementById('name-error')
 const emailError = document.getElementById('email-error')
 
+document.addEventListener('DOMContentLoaded', () => {
+    const container = document.querySelector('.card-body')
+
+    container.addEventListener('click', (event) => {
+        const clickBtn = event.target
+
+        if (clickBtn.classList.contains('edit-btn') || clickBtn.classList.contains('del-btn')) {
+            const psychologistId = clickBtn.dataset.id;
+
+            if (psychologistId) {
+                if (clickBtn.classList.contains('edit-btn')) {
+                    console.log('Editar psicólogo con ID: ' + psychologistId)
+                } else if (clickBtn.classList.contains('del-btn')) {
+                    console.log('Eliminar psicólogo con ID: ' + psychologistId)
+
+                    Swal.fire({
+                        title: 'Estas seguro que quieres eliminar el afiliado?',
+                        showDenyButton: true,
+                        reverseButtons: true,
+                        confirmButtonText: 'Eliminar',
+                        denyButtonText: `Cancelar`,
+                      }).then((result) => {
+                        if (result.isConfirmed) {
+                          deletePsychologist(psychologistId)
+                        }
+                      })
+                }
+            } else {
+                console.error('ID no encontrado');
+            }
+        }
+    })
+});
+
+function deletePsychologist(id) {
+    fetch(`http://localhost:3800/consult/${id}`, {
+        method: 'DELETE'
+    })
+    .then((res) => {
+        if(res.status === 200) {
+            Swal.fire('Eliminado!', '', 'success')
+            window.location.href = 'http://localhost:3800/consult'
+        } else {
+            Swal.fire('Error, no es posible eliminar', '', 'error')
+        }
+    })
+    .catch(error => {
+        console.error("Error: ", error)
+    })
+}
+
+const addBtn = document.getElementById('add-btn')
 addBtn.addEventListener('click', () =>{
     nameError.textContent = ''
 
@@ -48,4 +100,4 @@ addBtn.addEventListener('click', () =>{
     .catch(error => {
         console.error("Error: ", error)
     })
-})
+});
